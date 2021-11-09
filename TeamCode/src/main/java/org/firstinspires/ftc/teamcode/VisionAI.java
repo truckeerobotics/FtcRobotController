14644eb01cfb21fdb2ef533b4dc16327c87ed54f
Alexc96161 <alexc96161@gmail.com>
2021-11-08 17:15:31 -0800

    Camera code for VisionAI.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionAI.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionAI.java
@@ -4,22 +4,35 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import java.util.List;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
 
 // the beginning of the teleop mode
 
 @TeleOp(name = "AI vision", group = "Concept")
 
-// defining objects that the camera can see
-
 public class VisionAI extends LinearOpMode {
+
+    EmptyPipeline pipeline = new EmptyPipeline();
 
     @Override
     public void runOpMode() {
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+
+        // Get the camera and open it up. Send to inCameraOpenSuccessResult or inCameraOpenErrorResult depending on if opening was successful
+        // This is an Asynchronous function call, so when it is done opening it will call the function depending on result.
+        OpenCvCamera camera = getExternalCamera();
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override public void onOpened() { inCameraOpenSuccessResult(camera); }
+            @Override public void onError(int errorCode) { inCameraOpenErrorResult(errorCode); }
+        });
 
         waitForStart();
 
@@ -30,4 +43,34 @@ public class VisionAI extends LinearOpMode {
         }
     }
 
+    // Get the webcam (External camera)
+    public OpenCvCamera getExternalCamera() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        return OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+    }
+
+    // If the camera was opened up, then configure it, and then let pipeline deal with it.
+    public void inCameraOpenSuccessResult(OpenCvCamera camera) {
+        camera.setViewportRenderer(OpenCvCamera.ViewportRenderer.GPU_ACCELERATED);
+        camera.startStreaming(1920, 1080, OpenCvCameraRotation.UPRIGHT);
+        camera.setPipeline(pipeline);
+    }
+
+    // If camera had an error when trying to be opened.
+    public void inCameraOpenErrorResult(int errorCode) {
+
+    }
+
+}
+
+
+// Pipe :)
+class EmptyPipeline extends OpenCvPipeline
+{
+    @Override
+    public Mat processFrame(Mat input)
+    {
+        return input;
+    }
 }
