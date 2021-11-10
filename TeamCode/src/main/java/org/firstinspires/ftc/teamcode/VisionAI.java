cd8659511dfc991523b0795e0b1f8e7dc8d2967f
Alexc96161 <alexc96161@gmail.com>
2021-11-09 19:23:09 -0800

    Added comments to pipe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionAI.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionAI.java
@@ -10,6 +10,7 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
@@ -79,8 +80,8 @@ class ThresholdPipeline extends OpenCvPipeline
     public Scalar lower = new Scalar(0, 0, 0);
     public Scalar upper = new Scalar(255, 255, 100);
 
-    private Mat ycrcbMat       = new Mat();
-    private Mat binaryMat      = new Mat();
+    private Mat ycrcbMat = new Mat();
+    private Mat binaryMat = new Mat();
     private Mat maskedInputMat = new Mat();
 
     Telemetry telemetry;
@@ -93,16 +94,27 @@ class ThresholdPipeline extends OpenCvPipeline
     public Mat processFrame(Mat input)
     {
 
+        // Set the "color space" of the Mat (image)
         Imgproc.cvtColor(input, ycrcbMat, Imgproc.COLOR_RGB2YCrCb);
+
+        // Go through and get if in range (Color wise). All colors out of range will be 0, all in-range will be 1.
+        // Results in a Mat with 1's and 0's (Binary)
         Core.inRange(ycrcbMat, lower, upper, binaryMat);
+
+        // Release (Remove) the previous Mat.
         maskedInputMat.release();
+
+        // Make all the pixels that are 0's black (On the binary Mat), and no change for all that are 1's (On the binary Mat)
         Core.bitwise_and(input, input, maskedInputMat, binaryMat);
 
+        // Some debug.
         telemetry.addData("[processFrame]", "processed");
         telemetry.addData("[Lower Scalar]", lower);
         telemetry.addData("[Upper Scalar]", upper);
         telemetry.update();
 
-        return binaryMat;
+        return maskedInputMat;
     }
+
+
 }
